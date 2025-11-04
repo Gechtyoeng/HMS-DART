4a999f7073513b287b42d2cda7adea35b5401422
vesyean <veyseankem@gmail.com>
2025-11-04 12:27:04 +0700

    Implement all function in HMS  with mockup data

---
+++ hms_project/bin/main.dart
@@ -0,0 +1,12 @@
+// import '../lib/data/patientRepository.dart';
+// import '../lib/domian/hms.dart';
+import 'package:hms_project/ui/room_allocation.dart';
+
+void main() {
+  // Patientrepository patientList = Patientrepository();
+  // patientList.readPatients(
+  //   "D:/Y3/Develop/Molibe/HMS-DART/hms_project/lib/data/patientsList.json",
+  // );
+  RoomAllocation console = RoomAllocation();
+  console.startRoomAllocation();
+}
