67687a62e2340f43d8a9efba38f0eb1bd68cf2a3
vesyean <veyseankem@gmail.com>
2025-11-05 12:20:41 +0700

    Read and write data with JSON

--- hms_project/bin/main.dart
+++ hms_project/bin/main.dart
@@ -1,12 +1,24 @@
-// import '../lib/data/patientRepository.dart';
-// import '../lib/domian/hms.dart';
+import 'package:hms_project/data/hms_repository.dart';
+import 'package:hms_project/domian/hms.dart';
 import 'package:hms_project/ui/room_allocation.dart';
 
+// void main() {
+
+//   RoomAllocation console = RoomAllocation();
+//   console.startRoomAllocation();
+// }
+
 void main() {
-  // Patientrepository patientList = Patientrepository();
-  // patientList.readPatients(
-  //   "D:/Y3/Develop/Molibe/HMS-DART/hms_project/lib/data/patientsList.json",
-  // );
-  RoomAllocation console = RoomAllocation();
+  final hms = HMS(
+    hospitalName: "CADT",
+    location: "Prek Leab",
+    contact: "012345678",
+  );
+
+  // Load from JSON files
+  loadFromJson(hms);
+
+  // Launch the console UI
+  final console = RoomAllocation(hms);
   console.startRoomAllocation();
 }
